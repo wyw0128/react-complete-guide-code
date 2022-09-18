8d02e2820a4cfb919a235f8726a25c929059bab9
wyw0128 <yiweiwu0128@gmail.com>
2022-09-18 23:50:17 +1000

    output conditional content

--- react-complete-guide-code-05-rendering-lists-conditional-content/code/04-outputting-conditional-content/src/components/Expenses/Expenses.js
+++ react-complete-guide-code-05-rendering-lists-conditional-content/code/04-outputting-conditional-content/src/components/Expenses/Expenses.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
-import ExpensesFilter from './ExpensesFilter';
-import './Expenses.css';
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
@@ -31,11 +31,12 @@ const Expenses = (props) => {
 
   return (
     <div>
-      <Card className='expenses'>
+      <Card className="expenses">
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        {/* NOTE: react can render this kind of structure [<component>,<component>...]  */}
         {expensesContent}
       </Card>
     </div>
