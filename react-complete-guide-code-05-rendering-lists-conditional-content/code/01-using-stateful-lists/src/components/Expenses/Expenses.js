927ea4830b721f4912bee7cdb4ce266f148481a8
wyw0128 <yiweiwu0128@gmail.com>
2022-09-18 23:29:19 +1000

    learn stateful and stateless components

--- react-complete-guide-code-05-rendering-lists-conditional-content/code/01-using-stateful-lists/src/components/Expenses/Expenses.js
+++ react-complete-guide-code-05-rendering-lists-conditional-content/code/01-using-stateful-lists/src/components/Expenses/Expenses.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
-import ExpensesFilter from './ExpensesFilter';
-import './Expenses.css';
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
@@ -14,11 +14,12 @@ const Expenses = (props) => {
 
   return (
     <div>
-      <Card className='expenses'>
+      <Card className="expenses">
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        {/* NOTE: React will render array of JSX elements side by side, such as {[<Card/>,<Card/>]}. */}
         {props.items.map((expense) => (
           <ExpenseItem
             title={expense.title}
