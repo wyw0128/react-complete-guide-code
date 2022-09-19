f73ddfda9946447275fc781250402b4057bb9b38
wyw0128 <yiweiwu0128@gmail.com>
2022-09-19 18:25:11 +1000

    set dynamic inline styles using style property

--- react-complete-guide-code-06-styling/code/02-setting-dynamic-inline-styles/src/components/CourseGoals/CourseInput/CourseInput.js
+++ react-complete-guide-code-06-styling/code/02-setting-dynamic-inline-styles/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
+    // NOTE: This is to re-evaluate the entered value when the user changed the value.
     if (event.target.value.trim().length > 0) {
       setIsValid(true);
     }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
     if (enteredValue.trim().length === 0) {
       setIsValid(false);
@@ -26,11 +27,13 @@ const CourseInput = props => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
+        {/* NOTE: We use the style prop. We pass a object to it as demanded, but we dynamically changed the value that's fed into the color property in that style object. */}
+        {/* NOTE: But this inline style would overwrite other css styles and that is not good. */}
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
         <input
           style={{
-            borderColor: !isValid ? 'red' : '#ccc',
-            background: !isValid ? 'salmon' : 'transparent'
+            borderColor: !isValid ? "red" : "#ccc",
+            background: !isValid ? "salmon" : "transparent",
           }}
           type="text"
           onChange={goalInputChangeHandler}
