cf882b578afec120ff24481d6a4b94cc3662ef28
wyw0128 <yiweiwu0128@gmail.com>
2022-09-20 13:44:12 +1000

    add props to styled component

--- react-complete-guide-code-06-styling/code/05-styled-components-dynamic-props/src/components/CourseGoals/CourseInput/CourseInput.js
+++ react-complete-guide-code-06-styling/code/05-styled-components-dynamic-props/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
 const FormControl = styled.div`
   margin: 0.5rem 0;
@@ -11,14 +11,15 @@ const FormControl = styled.div`
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
-    color: ${props => (props.invalid ? 'red' : 'black')};
+    // NOTE: With this, we're having this dynamic way of changing parts of the styles based on some props that are passed to our styled component.
+    color: ${(props) => (props.invalid ? "red" : "black")};
   }
 
   & input {
     display: block;
     width: 100%;
-    border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
-    background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+    background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
@@ -31,18 +32,18 @@ const FormControl = styled.div`
   }
 `;
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
     if (event.target.value.trim().length > 0) {
       setIsValid(true);
     }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
     if (enteredValue.trim().length === 0) {
       setIsValid(false);
@@ -53,6 +54,7 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
+      {/* NOTE: We can pass props to this styled component to dynamically change the style inside the styled component. */}
       <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
