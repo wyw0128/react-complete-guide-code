6153a83ac4f2e3eb5e56835c33b04badac223546
wyw0128 <yiweiwu0128@gmail.com>
2022-09-21 14:11:25 +1000

    features of button component

--- react-complete-guide-code-07-debugging/code/02-finished/src/components/UI/Button/Button.js
+++ react-complete-guide-code-07-debugging/code/02-finished/src/components/UI/Button/Button.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import styles from './Button.module.css';
+import styles from "./Button.module.css";
 
-const Button = props => {
+const Button = (props) => {
+  // console.log("🚀 ~ file: Button.js ~ line 4 ~ styles", styles);
   return (
+    // NOTE: This button component is a general component, so it has onClick property. But when it was used inside a form, there is no need to use onClick because form is special.
+    // NOTE: The button element has a default type of submit. You can make it do nothing by setting a type of 'button'.
     <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
