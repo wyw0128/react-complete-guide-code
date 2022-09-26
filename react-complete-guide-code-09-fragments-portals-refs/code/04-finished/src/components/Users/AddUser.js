c3385d4a74d039affe8aec836017c152608f9abe
wyw0128 <yiweiwu0128@gmail.com>
2022-09-26 17:15:21 +1000

    how to use refs

--- react-complete-guide-code-09-fragments-portals-refs/code/04-finished/src/components/Users/AddUser.js
+++ react-complete-guide-code-09-fragments-portals-refs/code/04-finished/src/components/Users/AddUser.js
@@ -1,12 +1,13 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef } from "react";
 
-import Card from '../UI/Card';
-import Button from '../UI/Button';
-import ErrorModal from '../UI/ErrorModal';
-import Wrapper from '../Helpers/Wrapper';
-import classes from './AddUser.module.css';
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  // NOTE: This ref value, which is being generated here always is an object, which always has a current prop and the current prop holds the actual value that ref is connected with.
   const nameInputRef = useRef();
   const ageInputRef = useRef();
 
@@ -14,25 +15,27 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    // NOTE: useState is a hook which is used to update the state in functional component. useRef is a hook which provides a way to access to the DOM. It returns a mutable ref object so you can access the value using .current property.
+    // NOTE: And if you only want to read a value and you never plan on changing anything, well, then you don't really need state because just to use state as a keylogger is not that great. It's a lot of unnecessary code and work. So if you just want to read a value, refs are probably better.
     const enteredName = nameInputRef.current.value;
     const enteredUserAge = ageInputRef.current.value;
     if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
-        title: 'Invalid input',
-        message: 'Please enter a valid name and age (non-empty values).',
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
       });
       return;
     }
     if (+enteredUserAge < 1) {
       setError({
-        title: 'Invalid age',
-        message: 'Please enter a valid age (> 0).',
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0).",
       });
       return;
     }
     props.onAddUser(enteredName, enteredUserAge);
-    nameInputRef.current.value = '';
-    ageInputRef.current.value = '';
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
   const errorHandler = () => {
