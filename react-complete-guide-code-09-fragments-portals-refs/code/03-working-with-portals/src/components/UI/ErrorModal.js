8305facce6fd546d9b172e52b3b99d14c4d2fa40
wyw0128 <yiweiwu0128@gmail.com>
2022-09-26 15:56:22 +1000

    how to use portals

--- react-complete-guide-code-09-fragments-portals-refs/code/03-working-with-portals/src/components/UI/ErrorModal.js
+++ react-complete-guide-code-09-fragments-portals-refs/code/03-working-with-portals/src/components/UI/ErrorModal.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+// NOTE: So React DOM is kind of the adapter for React to the browser
+import ReactDOM from "react-dom";
 
-import Card from './Card';
-import Button from './Button';
-import classes from './ErrorModal.module.css';
+import Card from "./Card";
+import Button from "./Button";
+import classes from "./ErrorModal.module.css";
 
 const Backdrop = (props) => {
   return <div className={classes.backdrop} onClick={props.onConfirm} />;
@@ -28,9 +29,10 @@ const ModalOverlay = (props) => {
 const ErrorModal = (props) => {
   return (
     <React.Fragment>
+      {/* NOTE: Now the createPortal method takes two arguments. The first one is your React node that should be rendered. The second argument of createPortal is a pointer to that container in the real DOM where this elements should be rendered in.*/}
       {ReactDOM.createPortal(
         <Backdrop onConfirm={props.onConfirm} />,
-        document.getElementById('backdrop-root')
+        document.getElementById("backdrop-root")
       )}
       {ReactDOM.createPortal(
         <ModalOverlay
@@ -38,7 +40,7 @@ const ErrorModal = (props) => {
           message={props.message}
           onConfirm={props.onConfirm}
         />,
-        document.getElementById('overlay-root')
+        document.getElementById("overlay-root")
       )}
     </React.Fragment>
   );
