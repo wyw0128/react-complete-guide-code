c0e7f1f82ba8b7f48bf85bd7e09daf6c429fbc67
wyw0128 <yiweiwu0128@gmail.com>
2022-09-28 17:37:16 +1000

    use useEffect with dependencies

--- react-complete-guide-code-10-side-effects-reducers-context-api/code/03-useeffect-with-dependencies/src/components/Login/Login.js
+++ react-complete-guide-code-10-side-effects-reducers-context-api/code/03-useeffect-with-dependencies/src/components/Login/Login.js
@@ -1,20 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
   const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
-
+  // NOTE: useEffect in general, is a super important hook that helps you deal with code that should be executed in response to something. And something could be the component being loaded. It could be the email address being updated. It could be anything, whenever you have an action that should be executed in response to some other action, that is a side effect and that is where a useEffect is able to help you.
   useEffect(() => {
     setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      enteredEmail.includes("@") && enteredPassword.trim().length > 6
     );
+    // NOTE: A simple rule is you add as dependencies, what you're using in your side effect function.
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
@@ -26,7 +27,7 @@ const Login = (props) => {
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    setEmailIsValid(enteredEmail.includes("@"));
   };
 
   const validatePasswordHandler = () => {
@@ -43,7 +44,7 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
@@ -57,7 +58,7 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
