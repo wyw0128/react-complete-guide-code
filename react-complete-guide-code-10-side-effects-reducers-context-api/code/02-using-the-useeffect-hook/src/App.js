89bc242b3a30fc8cbea3046b6077b66156ed1969
wyw0128 <yiweiwu0128@gmail.com>
2022-09-28 17:19:54 +1000

    use useEffect with empty dependency

--- react-complete-guide-code-10-side-effects-reducers-context-api/code/02-using-the-useeffect-hook/src/App.js
+++ react-complete-guide-code-10-side-effects-reducers-context-api/code/02-using-the-useeffect-hook/src/App.js
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  // NOTE: So therefore this anonymous function here would indeed only run once when the app starts because thereafter the dependencies never change because this year specifically has no dependencies.
   useEffect(() => {
-    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
 
-    if (storedUserLoggedInInformation === '1') {
+    if (storedUserLoggedInInformation === "1") {
       setIsLoggedIn(true);
     }
   }, []);
@@ -18,12 +18,13 @@ function App() {
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn', '1');
+    // NOTE: To avoid losing data if click refresh, we can store data using localStorage.setItem to store those data, and we need 2 parameters, key and value pair; we can also get those data using getItem by providing only one parameter, which is the key.
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
