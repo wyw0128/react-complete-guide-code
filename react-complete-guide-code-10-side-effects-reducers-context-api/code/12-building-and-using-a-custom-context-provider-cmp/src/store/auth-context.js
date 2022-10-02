a8eea2b1c85b1b4f1838e357b2fffd896f9db8b2
wyw0128 <yiweiwu0128@gmail.com>
2022-10-02 18:12:51 +1100

    use customed context provider component

--- react-complete-guide-code-10-side-effects-reducers-context-api/code/12-building-and-using-a-custom-context-provider-cmp/src/store/auth-context.js
+++ react-complete-guide-code-10-side-effects-reducers-context-api/code/12-building-and-using-a-custom-context-provider-cmp/src/store/auth-context.js
@@ -1,29 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
   onLogout: () => {},
-  onLogin: (email, password) => {}
+  onLogin: (email, password) => {},
 });
-
+// NOTE: The advantage here could just be that you have a leaner app component which again is now just focused on returning JSX and bringing something onto the screen. And you have to actual off state management and auth-context management all in one file.
 export const AuthContextProvider = (props) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
 
-    if (storedUserLoggedInInformation === '1') {
+    if (storedUserLoggedInInformation === "1") {
       setIsLoggedIn(true);
     }
   }, []);
 
   const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
   const loginHandler = () => {
-    localStorage.setItem('isLoggedIn', '1');
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
