4ae5c4a60012ec1d5ee7cad7f5376808cf309ac8
wyw0128 <yiweiwu0128@gmail.com>
2022-10-12 22:44:59 +1100

    learn how to create custom hooks

--- react-complete-guide-code-15-building-custom-react-hooks/code/02-creating-a-custom-react-hook-function/src/hooks/use-counter.js
+++ react-complete-guide-code-15-building-custom-react-hooks/code/02-creating-a-custom-react-hook-function/src/hooks/use-counter.js
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react';
-
+import { useState, useEffect } from "react";
+// NOTE: This function name must start with use.
 const useCounter = () => {
+  // NOTE: If we use a custom hook in multiple components every component will receive its own state. So it's just a logic which has shared not the concrete state.
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
@@ -10,7 +11,7 @@ const useCounter = () => {
 
     return () => clearInterval(interval);
   }, []);
-
+  // NOTE: You can return whatever you wanna return in your custom hooks. That could be an array or an object or a number or in this case implicitly a number because counter will hold a number.
   return counter;
 };
 
