19703366a0e357b56bf15b170aefe4b3eb5b526d
wyw0128 <yiweiwu0128@gmail.com>
2022-10-13 09:38:06 +1100

    learn to build a custom http hook

--- react-complete-guide-code-15-building-custom-react-hooks/code/05-building-a-custom-http-hook/src/hooks/use-http.js
+++ react-complete-guide-code-15-building-custom-react-hooks/code/05-building-a-custom-http-hook/src/hooks/use-http.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const useHttp = (requestConfig, applyData) => {
   const [isLoading, setIsLoading] = useState(false);
@@ -8,6 +8,7 @@ const useHttp = (requestConfig, applyData) => {
     setIsLoading(true);
     setError(null);
     try {
+      // NOTE: Since we will be calling that hook, we then have to make sure that we pass in an object which has a URL property, which holds the URL.
       const response = await fetch(requestConfig.url, {
         method: requestConfig.method,
         headers: requestConfig.headers,
@@ -15,18 +16,19 @@ const useHttp = (requestConfig, applyData) => {
       });
 
       if (!response.ok) {
-        throw new Error('Request failed!');
+        throw new Error("Request failed!");
       }
 
       const data = await response.json();
       applyData(data);
     } catch (err) {
-      setError(err.message || 'Something went wrong!');
+      setError(err.message || "Something went wrong!");
     }
     setIsLoading(false);
   };
 
   return {
+    // NOTE: This is a modern JS shortcut, it is the same as {isLoading: isLoading, error: error...}
     isLoading,
     error,
     sendRequest,
