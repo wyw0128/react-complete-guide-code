a2c1f46b381bf8480af37c63ac7346394ac5fb9a
wyw0128 <yiweiwu0128@gmail.com>
2022-10-13 11:11:23 +1100

    learn to adjust custom hooks

--- react-complete-guide-code-15-building-custom-react-hooks/code/06-adjusting-the-custom-hook/src/App.js
+++ react-complete-guide-code-15-building-custom-react-hooks/code/06-adjusting-the-custom-hook/src/App.js
@@ -1,18 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
-import useHttp from './hooks/use-http';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+
+  // NOTE: There are cases where you want the destructured variable to have a different name than the property name; in that case, you'll use a : newName to specify a name for the variable, like in this case, the fetchTasks.
 
   const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
     const transformTasks = (tasksObj) => {
       const loadedTasks = [];
-
+      // NOTE: The for...in statement iterates over all enumerable string properties of an object (ignoring properties keyed by symbols), including inherited enumerable properties.
       for (const taskKey in tasksObj) {
         loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
       }
@@ -21,7 +23,7 @@ function App() {
     };
 
     fetchTasks(
-      { url: 'https://react-http-6b4a6.firebaseio.com/tasks.json' },
+      { url: "https://react-http-6b4a6.firebaseio.com/tasks.json" },
       transformTasks
     );
   }, [fetchTasks]);
